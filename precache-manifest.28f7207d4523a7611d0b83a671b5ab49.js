0a40f4556843f7e9d9f38e18013d403a22b847c9
Avery Peterson <averypeterson115@gmail.com>
2020-01-24 12:44:39 -0500

    Updates

---
+++ precache-manifest.28f7207d4523a7611d0b83a671b5ab49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8430504e3f5a20f88ec9cb10e926f18e",
+    "url": "/swiftshirt-frontend/index.html"
+  },
+  {
+    "revision": "9b5242d1a80cb4a6e4d0",
+    "url": "/swiftshirt-frontend/static/css/main.a1f7d441.chunk.css"
+  },
+  {
+    "revision": "3cdd32a4f4882244bb16",
+    "url": "/swiftshirt-frontend/static/js/2.f254f828.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/swiftshirt-frontend/static/js/2.f254f828.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9b5242d1a80cb4a6e4d0",
+    "url": "/swiftshirt-frontend/static/js/main.9295a1bd.chunk.js"
+  },
+  {
+    "revision": "6a360a306a4e8cceb8b3",
+    "url": "/swiftshirt-frontend/static/js/runtime-main.286b538a.js"
+  }
+]);
