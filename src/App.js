140a83f9177a700e3f8a152a0244f4f975568d60
Avery Peterson <averypeterson115@gmail.com>
2020-02-15 17:41:15 -0500

    create basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import './App.css';
-import 'bootstrap';
 
 import React from 'react';
+import api from './api';
+import logo from './swift_shirt_logo.png';
+import moment from 'moment';
+import styled from 'styled-components';
+import workouts from './workouts.json';
 
-// import api from './api';
+const Image = styled.img`
+	border-radius: 8px;
+	max-width: 100%;
+	max-height: 100%;
+	background-image: url(${logo});
+`;
+
+const ImageContainer = styled.div`
+	width: 300px;
+	height: 65px;
+	margin: 20px auto;
+	display: block;
+`;
+
+const BodyContainer = styled.div`border-top: 1px solid black;`;
+
+const WorkoutContainer = styled.div`margin-top: 30px;`;
+
+const Card = styled.div`
+	height: 200px;
+	background-color: black;
+	color: white;
+	border-radius: 15px;
+`;
+
+const CardText = styled.div`margin: auto;`;
 
 // import jQuery from 'jquery';
 
 // window.jQuery = window.$ = jQuery;
 
 function App() {
-	// api.get_workout();
-	//   .then((resp) => {
-	// 	console.log(resp);
-	// });
+	api.get_workout().then((resp) => {
+		console.log(resp);
+	});
+
+	let workoutCards = [];
+
+	for (let i = 0; i <= workouts.length - 1; i++) {
+		workoutCards.push(
+			<div className="col-md-4">
+				<Card className="card mb-4">
+					<CardText>
+						<div>{workouts[i].name}</div>
+						<div>{moment(workouts[i].name, 'DD/MM/YYYY').format('dddd, MMMM Do YYYY')}</div>
+					</CardText>
+				</Card>
+			</div>
+		);
+	}
 
 	return (
 		<div className="App">
 			<header className="App-header">
-				<p>SWIFT Shirt Frontend</p>
+				<ImageContainer>
+					<Image src={logo} alt="Swift Shirt Logo" />
+				</ImageContainer>
 			</header>
+			<BodyContainer>
+				<WorkoutContainer className="container">
+					<div className="row">{workoutCards}</div>
+				</WorkoutContainer>
+			</BodyContainer>
 		</div>
 	);
 }
--- src/api.js
+++ src/api.js
@@ -1,40 +1,16 @@
 // import store from './store';
-// import jQuery from 'jquery';
+import jQuery from 'jquery';
 
 class Server {
 	get_workout() {
-		// const Http = new XMLHttpRequest();
-		// const url = 'http://swiftshirt.us-east-2.elasticbeanstalk.com/api/v1/workout/1';
-		// Http.open('GET', url);
-		// Http.send();
-		// Http.onreadystatechange = (e) => {
-		// 	console.log(Http.responseText);
-		// };
-		// return;
-		// return jQuery.ajax('http://swiftshirt.us-east-2.elasticbeanstalk.com/api/v1/workout/1', {
-		// 	method: 'get',
-		// 	dataType: 'json',
-		// 	contentType: 'application/json; charset=UTF-8',
-		// 	headers: {
-		// 		'Access-Control-Allow-Origin': 'http://localhost:3000/',
-		// 		'Access-Control-Allow-Credentials': true
-		// 	},
-		// 	crossDomain: true
-		// 	success: (resp) => {
-		// 		// 	// store.dispatch({
-		// 		// 	// 	type: 'NEW_RECS',
-		// 		// 	// 	data: resp
-		// 		// 	// });
-		// 		// 	// store.dispatch({
-		// 		// 	// 	type: 'NEW_SPOTIFY_PLAYER',
-		// 		// 	// 	data: {
-		// 		// 	// 		spotifyType: 'track',
-		// 		// 	// 		spotifyId: resp.rec.id
-		// 		// 	// 	}
-		// 		// 	// });
-		// 		console.log(resp);
-		// }
-		// });
+		return jQuery.ajax('http://swiftshirt.us-east-2.elasticbeanstalk.com/api/v1/workout/1', {
+			method: 'get',
+			dataType: 'json',
+			contentType: 'application/json; charset=UTF-8',
+			success: (resp) => {
+				console.log(resp);
+			}
+		});
 	}
 }
 
